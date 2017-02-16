f64dc9daed0e5cbda4df6f9e9699e8693e242999
Steven <steven.castroverde@gmail.com>
2017-02-16 16:15:24 -0700

    vr failure

--- public/app/camera/camera.controller.js
+++ public/app/camera/camera.controller.js
@@ -8,21 +8,6 @@
 
     function CameraController() {
         const vm = this;
-        vm.$onInit = function() {
-            var container = document.getElementById('vr-canvas');
-            var canvas = document.createElement("canvas");
-            container.appendChild(canvas);
-
-            // Create h264 player
-            var uri = "ws://" + document.location.host;
-            var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
-            wsavc.connect(uri);
-
-            //expose instance for button callbacks
-            window.wsavc = wsavc;
-        }
-
-
     }
 
 })();
--- public/app/vr/vr.controller.js
+++ public/app/vr/vr.controller.js
@@ -10,6 +10,23 @@
         const vm = this;
         vm.$onInit = function() {}
 
+        var container = document.getElementById('vr-canvas');
+        var container2 = document.getElementById('vr-canvas2');
+        var canvas = document.createElement("canvas");
+        var canvas2 = document.createElement("canvas");
+        container.appendChild(canvas);
+        container2.appendChild(canvas);
+
+        // Create h264 player
+        var uri = "ws://" + document.location.host;
+        var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
+        var wsavc2 = new WSAvcPlayer(canvas2, "webgl", 1, 35);
+        wsavc.connect(uri);
+        wsavc2.connect(uri);
+
+        //expose instance for button callbacks
+        window.wsavc = wsavc;
+        window.wsavc2 = wsavc2;
     }
 
 })();
