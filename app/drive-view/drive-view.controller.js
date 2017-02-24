94eaebc2e352f0dce8eac6e80620a9ea7e4a6445
Murph Grainger <murph.grainger@gmail.com>
2017-02-24 10:09:33 -0700

    initial deployment commit

---
+++ app/drive-view/drive-view.controller.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('DriveViewController', DriveViewController);
+
+
+    function DriveViewController($window) {
+        const vm = this;
+        vm.$onInit = function() {
+
+        }
+
+
+    }
+
+})();
